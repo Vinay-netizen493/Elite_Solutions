dc928f16b83deb462c976ba981d0b81c3ca4a3a0
Vinay <85448141+Vinay-netizen493@users.noreply.github.com>
2023-01-27 16:25:18 +0530

    Create Balanced_BinaryTree.java

---
+++ Balanced_BinaryTree.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null)
+            return true;
+        if(height(root)==-1)
+            return false;
+        return true;
+    }
+    public int height(TreeNode root){
+        if(root==null)
+            return 0;
+        int lh=height(root.left);
+        int rh=height(root.right);
+        if((Math.abs(lh-rh)>1) || lh==-1 || rh==-1)
+            return -1;
+        return Math.max(lh,rh)+1;
+    }
+}
