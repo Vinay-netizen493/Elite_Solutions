d6ec738979f56bf0d0894d560e76eca9f952cdea
Vinay <85448141+Vinay-netizen493@users.noreply.github.com>
2023-01-28 15:27:26 +0530

    Create BST_Minimum_AbsoluteDifference.java

---
+++ BST_Minimum_AbsoluteDifference.java
@@ -0,0 +1,22 @@
+ class Solution {
+    public int getMinimumDifference(TreeNode root) {
+         List<Integer> ls=new LinkedList<>();
+        int min=Integer.MAX_VALUE;
+        inorder(root,ls);
+        for(int i=0;i<ls.size()-1;i++){
+            int m=ls.get(i+1)-ls.get(i);
+            min=Math.min(m,min);
+        }
+        return min;
+    }
+    public void inorder(TreeNode root,List<Integer> ls){
+        
+        
+        if(root==null)
+            return ;
+        inorder(root.left,ls);
+        ls.add(root.val);
+        inorder(root.right,ls);
+        
+    }
+}
